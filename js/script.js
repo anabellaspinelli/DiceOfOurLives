684877f1cf3b833e21f140b554676bd49f00c46c
Ana <anabellaspinelli@gmail.com>
2014-10-01 21:51:15 -0300

    Dalmirog corrections to the script (v2)

--- js/script.js
+++ js/script.js
@@ -1,73 +1,54 @@
-var sides = 6 /*Die simulator initializes with a 6-sided die*/
-var shape = "square" /*6-sided die are squares*/
-var backgroundColors = ["#DFDFDF", "#fbeed5","#f2dede","#B6C9D8","#dff0d8", '#d9edf7']; /*Possible background colors*/
-var diceShapes = ["whiteTriangle", "square", "redTriangle", "kite", "pentagon", "blueTriangle"] /*Posible die shapes*/
+var sides = 6; //Die simulator initializes with a 6-sided die
+var shape = "square"; //6-sided die are squares
+var backgroundColors = ["#DFDFDF", "#fbeed5","#f2dede","#B6C9D8","#dff0d8", '#d9edf7']; //Possible background colors
+var diceShapes = ["whiteTriangle", "square", "redTriangle", "kite", "pentagon", "blueTriangle"]; //Posible die shapes
 
-
-/*Remove initial css animation class*/
-var removeCssAnimation = function() {
-	if ($("#die").hasClass("bigEntrance")) {
-		$("#die").removeClass("bigEntrance");
-	}
-}
-
-/*Dice randomizing function*/
-var rollDice = function() { 
+//Dice randomizing function
+function rollDice() { 
 	var number = Math.floor(Math.random() * sides) + 1;
-	$("#die").attr("src", "img/" + number + shape + ".png");
+	$('#die').attr("src", "img/" + number + shape + ".png");
+	
 }
 
-/*Initialize jRumble on die element*/
-var initializeRumble = function() {
-	$('#die').jrumble({
-		x: 10,
-		y: 10,
-		rotation: 4
-	});
+function initializeRumble(){
+	$('#die').jrumble();
+};
+
+function animation () {
+	$("#die").trigger('startRumble');
+	setTimeout(function(){$('#die').trigger('stopRumble');}, 500);
+	};
+
+//Changes the number of sides for the current die
+ function shapeChanger (senderObject){
+	sides = $(senderObject).attr('id'); //Takes the button number id and uses it to randomize the dice
+
+	var buttonNumber = $(senderObject).attr('name'); //Defines local variable and assings button "name" attribute's numeric value
+	shape = diceShapes[buttonNumber];
+	$('body').css("background-color", backgroundColors[buttonNumber]);	//Changes the background color to match the die
 }
 
-/*Animate die roll on each click using jRumble, code taken from http://jackrugile.com/jrumble/*/
-var animation= function() {
-	var demoTimeout;
-	$('.diceButton , #die').click(function(){
-		clearTimeout(demoTimeout);
-		$("#die").trigger('startRumble');
-		demoTimeout = setTimeout(function(){$('#die').trigger('stopRumble');}, 500)
-	});
-}
+///Events
 
-/*Changes the number of sides for the current die*/
-var shapeChanger = function(senderObject){
-	sides = $(senderObject).attr('id'); /*Takes the button number id and uses it to randomize the dice*/
-	var buttonNumber = $(senderObject).attr('name'); /*Defines local variable and assings button "name" attribute's numeric value*/
-	shape = diceShapes[buttonNumber]; /*Uses "name" attribute numerica value select a die shape from the shapes array*/
-	$("body").css("background-color", backgroundColors[buttonNumber]);	/*Changes the background color to match the die*/
-}
+//Click on dice to roll again
+$('#die').click(function() {
+	rollDice();
+	animation();
+});
 
+//Generic dice button function
+$('.diceButton').click(function() {
+	shapeChanger(this);
+	rollDice();
+	animation();
+});
 
-
-
-
+//PageLoad event
 $(document).ready(function() {
-	initializeRumble();
-	animation(); //Animate when page loads
+	initializeRumble(); //Loads jrumble on $('#die') object
+	//animation(); //Animate when page loads //Lo comente porque me parece que queda mejor que no vibre cuando hace la animacion inicial.
 	rollDice(); //Roll dice when page loads
 
-
-	//Click on dice to roll again
-	$("#die").click(function() {
-		removeCssAnimation(); /*Remove css page load animation so it doesn't collide with rumble animation*/
-		rollDice();
-		animation();
-	});
-
-	//Generic dice button function
-	$('.diceButton').click(function() {
-		var thisObject = $(this);
-		shapeChanger(thisObject);
-		rollDice();
-		animation();
-	});	
 });
 
 
